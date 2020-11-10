7aa94cfb50c84a1873d9ef1c064743305d484152
João Palma <joaowd@outlook.com>
2020-11-10 23:54:37 +0000

    Refactor JsonFileManager

--- MyWorkout/MyWorkout/Core/Services/Implementations/JsonFileManagerImp.swift
+++ MyWorkout/MyWorkout/Core/Services/Implementations/JsonFileManagerImp.swift
@@ -8,37 +8,67 @@
 import UIKit
 
 final class JsonFileManagerImp: JsonFileManager {
-    private let fileManager = FileManager.default
-    
-    func retrieveFromJsonFile<T: Codable>(fileName: String) -> T? {
+    func saveToJsonFile<T: Codable>(fileName: String, data: T) {
         do {
-            let documentDirectory = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            let url = documentDirectory.appendingPathComponent("\(fileName).json")
-            
-            guard fileManager.fileExists(atPath: url.path) else { return nil }
-                
-            let jsonData = try Data(contentsOf: url, options: .mappedIfSafe)
-            let json = try? JSONDecoder().decode(T.self, from: jsonData)
-            return json
-        } catch {
-            print(error)
-            fatalError("Unable to read file")
-        }
-    }
-    
-    func saveToJsonFile<T>(fileName: String, data: T) where T : Decodable, T : Encodable {
-        do {
-            let documentDirectory = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            let url = documentDirectory.appendingPathComponent("\(fileName).json")
+            let directory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            let file = directory.appendingPathComponent("\(fileName).json")
             
             let encoder = JSONEncoder()
-            encoder.outputFormatting = .prettyPrinted
             let jsonData = try encoder.encode(data)
-            
-            try jsonData.write(to: url)
+            try jsonData.write(to: file)
         } catch {
-            print(error)
             fatalError("Unable to write contents to file")
         }
     }
+    
+    func retrieveFromJsonFile<T: Codable>(fileName: String) -> T? {
+        do {
+            let directory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let file = directory.appendingPathComponent("\(fileName).json")
+            
+            guard FileManager.default.fileExists(atPath: file.path) else { return nil }
+                
+            let jsonData = try Data(contentsOf: file, options: .mappedIfSafe)
+            let json = try JSONDecoder().decode(T.self, from: jsonData)
+            return json
+        } catch {
+            fatalError("Unable to read file")
+        }
+    }
 }
+
+
+enum FileStore {
+    func saveToJsonFile<T: Codable>(fileName: String, data: T) {
+        do {
+            let directory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            
+            //iCloud
+            //let iCloudDirectory = try FileManager.default.url(forUbiquityContainerIdentifier: nil)
+            
+            let file = directory.appendingPathComponent("\(fileName).<#fileExt#>")
+            
+            let encoder = JSONEncoder()
+            let jsonData = try encoder.encode(data)
+            try jsonData.write(to: file)
+        } catch {
+            fatalError("Unable to write contents to file")
+        }
+    }
+    
+    func retrieveFromJsonFile<T: Codable>(fileName: String) -> T? {
+        do {
+            let directory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let file = directory.appendingPathComponent("\(fileName).<#fileExt#>")
+            
+            guard FileManager.default.fileExists(atPath: file.path) else { return nil }
+            
+            let jsonData = try Data(contentsOf: file, options: .mappedIfSafe)
+            let json = try JSONDecoder().decode(T.self, from: jsonData)
+            return json
+        } catch {
+            fatalError("Unable to read file")
+        }
+    }
+}
+
